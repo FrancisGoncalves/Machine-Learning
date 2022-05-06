2eb2cad3250112ff6793d30863870d1cd5f3ac39
FrancisGoncalves <88954913+FrancisGoncalves@users.noreply.github.com>
2022-05-05 19:22:29 -0700

    Add files via upload

---
+++ Perceptron/MultiPA.py
@@ -0,0 +1,92 @@
+# ------------------------------------------- WSU - CPT_S 570 - HW1 -----------------------------------------
+# --------------------- Online Multi-Class Classifier Learning Algorithm - Passive-Agressive -------------------
+# ----------------------------------------- Author: Francisco Goncalves ------------------------------------------
+#
+# This code is a function that runs the Passive Agressive algorithm for Multi-Class Classification and calculates
+# the accuracy and number of mistakes per iteration
+#
+# INPUTS:
+#   T - Number of iterations (integer)
+#   accuracy_trigger - This variable activates or deactivates the computation of accuracy in order to save computational
+#                      time in case we just want the number of mistakes (0/1)
+# OUTPUTS:
+#   accuracy_multiPA_training - Accuracy on the training data per iteration
+#   accuracy_multiPA_testing - Accuracy on the testing data per iteration
+#   mistakes_multi_PA - Mistakes per training iteration
+#
+from loadingData import D, E
+import numpy as np
+
+# Defining the function to be called in the main script
+def MultiPA(T, accuracy_trigger):
+    if accuracy_trigger == 1:
+        print("Please wait a few moments while accuracy for Multi-Class Classification PA is computed...\n")
+    else:
+        print("Please wait a few moments while Online Learning curve for Multi-Class Classification PA is computed...\n")
+    # Defining function that allocates example 'xt' to a particular position in a vector according to label 'yt'
+    def F(xt, yt):
+        d = np.zeros((1, 10 * int(xt.shape[0])))
+        d[0, yt * int(xt.shape[0]):784 * (yt + 1)] = xt
+        return d
+    # Initializing weight vector
+    w = np.zeros((1, 7840))
+    # Prealocating scores vector
+    scores = np.zeros((1, 10))
+    # Prealocating storage output vectors
+    mistakes_multi_PA = np.zeros((1, T))
+    accuracy_multiPA_training = np.zeros((1, T))
+    accuracy_multiPA_testing = np.zeros((1, T))
+    # Learning algorithm
+    for i in range(T):
+        # Defining number of mistakes as zero at the beginning of each iteration
+        training_mistakes = 0
+        # print("Iteration " + str(i + 1) + " of " + str(T))
+        for k in range(D[0].shape[0]):
+            for y in range(10):
+                # Computing scores for each label (0, 1, ..., 9)
+                scores[:, y] = np.inner(w[0, y * int(D[0].shape[1]):784 * (y + 1)], D[0][k, :])
+            # Computing the label prediction (label with highest score)
+            prediction = np.argmax(scores)
+            # If prediction is different from the actual label
+            if prediction != D[1][k]:
+                # Add one more mistake for this iteration
+                training_mistakes = training_mistakes + 1
+                # Updating learn rate 'tau'
+                tau = (1 - (np.inner(w[0, D[1][k] * 784:784 * (D[1][k] + 1)], D[0][k])-np.inner(w[0, prediction * 784:784 * (prediction + 1)], D[0][k])))/((np.linalg.norm(F(D[0][k], D[1][k]) - F(D[0][k], prediction)))**2)
+                # Updating weight vector
+                w = w + tau * (F(D[0][k], D[1][k]) - F(D[0][k], prediction))
+        # Storing number of mistakes for this iteration
+        mistakes_multi_PA[0, i] = training_mistakes
+        # If the accuracy trigger is activated
+        if accuracy_trigger == 1:
+            # Initializing number of correct predicted labels
+            correct_training = 0
+            # For all training examples
+            for j in range(D[0].shape[0]):
+                for y in range(10):
+                    scores[:, y] = np.inner(w[0, y * int(D[0].shape[1]):784 * (y + 1)], D[0][j, :])
+                # Predict label
+                prediction = np.argmax(scores)
+                # If prediction is correct
+                if prediction == D[1][j]:
+                    # Add one more correct prediction
+                    correct_training = correct_training + 1
+            # Storing accuracies (Total Correct Predictions/Number of training examples) for training data for
+            # this iteration
+            accuracy_multiPA_training[0, i] = correct_training/60000
+            # Initializing number of correct predicted labels
+            correct_testing = 0
+            # For all testing examples
+            for j in range(E[0].shape[0]):
+                for y in range(10):
+                    scores[:, y] = np.inner(w[0, y * int(E[0].shape[1]):784 * (y + 1)], E[0][j, :])
+                # Predict label
+                prediction = np.argmax(scores)
+                # If prediction is correct
+                if prediction == E[1][j]:
+                    # Add one more correct prediction
+                    correct_testing = correct_testing + 1
+            # Storing accuracies (Total Correct Predictions/Number of testing examples) for testing data for
+            # this iteration
+            accuracy_multiPA_testing[0, i] = correct_testing/10000
+    return accuracy_multiPA_training, accuracy_multiPA_testing, mistakes_multi_PA
---
+++ Perceptron/README.txt
@@ -0,0 +1,8 @@
+Programming language and version: Python 3.9
+
+Please install the following packages:
+- matplotlib
+- mlxtend
+- numpy
+
+To run the code please RUN: 'mainScript.py'
